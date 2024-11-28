22608ba608ec0e5731e8ea09b624ee07296223c9
Shinebayar G. <3091558+shinebayar-g@users.noreply.github.com>
2024-11-27 21:37:29 -0800

    feat: generate provider classes (#19)

---
+++ packages/provider-generator/src/class.ts
@@ -0,0 +1,45 @@
+import * as t from '@babel/types';
+import { toPascalCase } from './utils.js';
+
+function constructorParams(className: string): t.Identifier[] {
+    const configParam = t.identifier('config');
+    configParam.typeAnnotation = t.tsTypeAnnotation(
+        t.tsTypeReference(t.identifier('TerraformConfig')),
+    );
+
+    const nameParam = t.identifier('name');
+    nameParam.typeAnnotation = t.tsTypeAnnotation(t.tsStringKeyword());
+
+    const argsParam = t.identifier('args');
+    argsParam.typeAnnotation = t.tsTypeAnnotation(
+        t.tsTypeReference(t.identifier(`${toPascalCase(className)}Args`)),
+    );
+    return [configParam, nameParam, argsParam];
+}
+
+export function generateClassDeclaration(className: string) {
+    return t.exportNamedDeclaration(
+        t.classDeclaration(
+            t.identifier(className),
+            t.identifier('TerraformResource'),
+            t.classBody([
+                t.classMethod(
+                    'constructor',
+                    t.identifier('constructor'),
+                    constructorParams(className),
+                    t.blockStatement([
+                        t.expressionStatement(
+                            t.callExpression(t.super(), [
+                                t.identifier('config'),
+                                t.stringLiteral('resource'),
+                                t.identifier('args'),
+                                t.identifier('name'),
+                                t.stringLiteral(className),
+                            ]),
+                        ),
+                    ]),
+                ),
+            ]),
+        ),
+    );
+}
--- packages/provider-generator/src/generate.ts
+++ packages/provider-generator/src/generate.ts
@@ -3,21 +3,46 @@ import path from 'node:path';
 import generate from '@babel/generator';
 import * as t from '@babel/types';
 import chalk from 'chalk';
+import { generateClassDeclaration } from './class.js';
 import type { ProviderSchema } from './schema.js';
 
 export function generateProviders(providerSchemas: Record<string, ProviderSchema>) {
-    for (const [name, schema] of Object.entries(providerSchemas)) {
-        console.log(`Generating TypeScript code for provider: ${chalk.green(name)}`);
-        fs.rmdirSync(name, { recursive: true });
-        for (const [className, resourceBlock] of Object.entries(schema.resource_schemas)) {
+    for (const [providerName, schema] of Object.entries(providerSchemas)) {
+        console.log(`Generating TypeScript code for provider: ${chalk.green(providerName)}`);
+        fs.rmdirSync(providerName, { recursive: true });
+        fs.mkdirSync(providerName, { recursive: true });
+
+        for (const [resourceName, resourceBlock] of Object.entries(schema.resource_schemas)) {
             console.log(
-                `Generating TypeScript code for resource: ${chalk.yellowBright(className)}`,
+                `Generating TypeScript code for resource: ${chalk.yellowBright(resourceName)}`,
             );
-            const ast = t.program([], [], 'module');
+            const ast = t.program(
+                [
+                    t.importDeclaration(
+                        [
+                            t.importSpecifier(
+                                t.identifier('TerraformConfig'),
+                                t.identifier('TerraformConfig'),
+                            ),
+                            t.importSpecifier(
+                                t.identifier('TerraformResource'),
+                                t.identifier('TerraformResource'),
+                            ),
+                        ],
+                        t.stringLiteral('tfs'),
+                    ),
+                    // TODO: Generate Interfaces here
+                    generateClassDeclaration(resourceName),
+                ],
+                [],
+                'module',
+            );
             // @ts-expect-error
             const { code } = generate(ast, {});
-            fs.mkdirSync(name, { recursive: true });
-            fs.writeFileSync(path.join(name, `${className}.ts`), code, { encoding: 'utf8' });
+
+            fs.writeFileSync(path.join(providerName, `${resourceName}.ts`), code, {
+                encoding: 'utf8',
+            });
         }
     }
 }
