4e3d787c9bb672a2f135a16b74461b99a1c4d9d8
Shinebayar G. <3091558+shinebayar-g@users.noreply.github.com>
2024-11-27 16:13:35 -0800

    feat: add core terraform resources (#15)

---
+++ packages/tfs/src/core.ts
@@ -0,0 +1,60 @@
+import { TerraformConfig } from './config';
+import { TerraformResource } from './resource';
+
+interface TerraformArgs {
+    [key: string]: any;
+    backend: any;
+}
+
+export class Terraform extends TerraformResource {
+    constructor(config: TerraformConfig, args: TerraformArgs) {
+        super(config, 'resource', args, 'terraform', 'terraform');
+    }
+}
+
+interface TerraformModuleArgs {
+    source: string;
+    [key: string]: any;
+}
+
+export class Module extends TerraformResource {
+    constructor(config: TerraformConfig, name: string, args: TerraformModuleArgs) {
+        super(config, 'module', args, name);
+    }
+}
+
+interface ImportArgs {
+    to: string;
+    id: string;
+}
+
+export class Import extends TerraformResource {
+    constructor(config: TerraformConfig, args: ImportArgs) {
+        super(config, 'import', args);
+    }
+}
+
+interface MovedArgs {
+    from: string;
+    to: string;
+}
+
+export class Moved extends TerraformResource {
+    constructor(config: TerraformConfig, args: MovedArgs) {
+        super(config, 'moved', args);
+    }
+}
+
+interface CheckArgs {
+    assert: {
+        condition: boolean;
+        message: string;
+    };
+    data: TerraformResource;
+}
+
+export class Check extends TerraformResource {
+    constructor(config: TerraformConfig, args: CheckArgs) {
+        super(config, 'check', args);
+    }
+}
